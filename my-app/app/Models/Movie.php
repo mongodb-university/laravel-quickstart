e60c6bcde4fc55ab885424c0b7e85a0ba772f817
Chris Cho <chris.cho@mongodb.com>
2024-03-26 17:27:46 -0400

    DOCSP-38876: Laravel 11.x app files

--- my-app/app/Models/Movie.php
+++ my-app/app/Models/Movie.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace App\Models;
-
 use MongoDB\Laravel\Eloquent\Model;
 
 class Movie extends Model
 {
-  protected $connection = 'mongodb';
-  protected $fillable = ['title', 'year', 'runtime', 'imdb', 'plot'];
+    protected $connection = 'mongodb';
+    protected $fillable = ['title', 'year', 'runtime', 'imdb', 'plot'];
 }
